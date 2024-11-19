661deed58a69f5124406068a249a398dc3c3adf1
DudeBroGuy <600462@buchananschools.com>
2024-11-19 11:51:40 -0500

    y

--- ex03/script.js
+++ ex03/script.js
@@ -2,13 +2,13 @@
 function task1() {
   console.log("Running task 1");
   // Don't change the variable name
-  let listItems = // Set your selector here. ;
+  let listItems = document.querySelectorAll("li") // Set your selector here. ;
 
   // This is called a conditional. If it finds matches
   // for your variable, it will tell each of them to 
   // respond to a hover ("mouseover") event.
   if (listItems.length > 1) {
-    listItems.forEach((item) =>
+    listItems.forEach((item) => 
       item.addEventListener("mouseover", handleHover),
     );
   }
@@ -17,11 +17,14 @@ function task1() {
 // Write your task 2 code inside this function!
 function task2() {
   console.log("Running task 2");
+  const element = document.getElementById("circle");
+element.remove("circle"); 
 }
 
 // Write your task 3 code inside this function!
 function task3() {
   console.log("Running task 3");
+
 }
 
 // Don't change this function
